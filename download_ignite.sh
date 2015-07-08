9668013864595ca0250a7cdc72a42d4f9b46c10e
nikolay_tikhonov <ntikhonov@gridgain.com>
2015-07-08 18:14:58 +0300

    Added licence url.

--- download_ignite.sh
+++ download_ignite.sh
@@ -22,28 +22,36 @@ function download {
   unzip ignite.zip -d ignite
 
   rm ignite.zip
-
-  exit 0
 }
 
 if [ ! -z $IGNITE_URL ]; then
   download $IGNITE_URL
+
+  exit 0
 fi
 
 if [ ! -z $IGNITE_VERSION ]; then
   if [[ $IGNITE_VERSION  =~ [0-9]*\.[0-9]*\.0 ]]; then
     download http://apache-mirror.rbc.ru/pub/apache/incubator/ignite/${IGNITE_VERSION}/apache-ignite-fabric-${IGNITE_VERSION}-incubating-bin.zip
+
+    exit 0
   else
     download http://www.gridgain.com/media/gridgain-community-fabric-${IGNITE_VERSION}.zip
+
+    exit 0
   fi
 fi
 
 if [ -z $IGNITE_SOURCE ] || [ $IGNITE_SOURCE = "COMMUNITY" ]; then
   download http://tiny.cc/updater/download_community.php
+
+  exit 0
 fi
 
 if [ $IGNITE_SOURCE = "APACHE" ]; then
   download http://tiny.cc/updater/download_ignite.php
+
+  exit 0
 fi
 
 if [ ! -z $LICENCE_URL ]; then
